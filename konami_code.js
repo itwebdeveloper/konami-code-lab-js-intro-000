b4e4028ba7d5c8770971f30eb21a42e5d3163540
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-28 00:12:48 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,20 +10,20 @@ const codes = [
   "b",
   "a"
 ];
- 
+
 // Keep track of index outside of the event handler.
 let index = 0;
- 
+
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = e.key;
- 
+
   if (key === codes[index]) {
     index++;
- 
+
     if (index === codes.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
